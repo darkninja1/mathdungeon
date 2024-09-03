fbd31c324772ee4653c138aa105f67c2aca5cd48
Dominik Honzak <49328567+darkninja1@users.noreply.github.com>
2024-09-03 00:58:42 +0000

    ...

--- vite.config.js
+++ vite.config.js
@@ -3,8 +3,10 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  server: {
-    host: '0.0.0.0',
-  }
+    base:'/mathdungeon/',
+    plugins: [react()]
+  // plugins: [react()],
+  // server: {
+  //   host: '0.0.0.0',
+  // }
 })
